4493d1dee4cd689ea390364f35fde06127893a40
Andy Zheng <azheng@objectivasoftware.com>
2015-09-17 16:40:56 +0800

    little update

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   }
   render() {
   	return (
-  		<div>
+  		<div id="container">
   			<SideBar />
   			<RouteHandler />
   		</div>
--- js/components/SideBar.js
+++ js/components/SideBar.js
@@ -5,12 +5,14 @@ export default class SideBar extends React.Component {
 	constructor(args) {
 		super(args);
 	}
+
 	render() {
 		return (
 			<nav id="nav">
 				<ul>
 					<li><Link to="home">Home</Link></li>
 					<li><Link to="projectList">Projects</Link></li>
+					<li><a href="http://blog.codinglife.in">Blog</a></li>
 					<li><Link to="about">About</Link></li>
 				</ul>
 			</nav>
